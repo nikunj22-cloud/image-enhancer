a8767ecccc8921af5a3cba2d4ccee6d0bd88a33c
nikunj22-cloud <158015053+NikunjVarshney22@users.noreply.github.com>
2025-04-03 22:45:04 +0530

    aa

--- image/vite.config.js
+++ image/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  base: "./image", // अपने रिपोजिटरी नाम के अनुसार बदलें
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        tailwindcss(), // केवल TailwindCSS
-      ],
-    },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
+    postcss: "./postcss.config.js",
   },
 });
