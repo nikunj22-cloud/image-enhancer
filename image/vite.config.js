aa161d36ba4856539bd25c8d77595def848764c3
nikunj22-cloud <158015053+NikunjVarshney22@users.noreply.github.com>
2025-04-03 22:37:13 +0530

    gg

--- image/vite.config.js
+++ image/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // या अपने रिपोजिटरी नाम के अनुसार
+  base: "./image", // या अपने रिपोजिटरी नाम के अनुसार
   plugins: [react()],
   css: {
     postcss: {
