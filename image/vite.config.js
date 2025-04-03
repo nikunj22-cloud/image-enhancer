d2346288005ec3c6cda38271dc3104543f4a11e4
nikunj22-cloud <158015053+NikunjVarshney22@users.noreply.github.com>
2025-04-03 22:47:55 +0530

    de

--- image/vite.config.js
+++ image/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: "./postcss.config.cjs",
+    postcss: path.resolve(__dirname, "postcss.config.cjs"),
   },
 });
