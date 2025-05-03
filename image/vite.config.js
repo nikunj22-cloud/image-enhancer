fec40df1df0fdb64b09423ba5e22969ff6e5e7ed
nikunj22-cloud <158015053+NikunjVarshney22@users.noreply.github.com>
2025-05-03 21:23:02 +0530

    fix

--- image/vite.config.js
+++ image/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: ".", // Ensure the root is set to the current directory
+  root: "image", // Explicitly set the root directory
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
